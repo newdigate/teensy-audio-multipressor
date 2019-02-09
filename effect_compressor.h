d7abfb15433307f68a85f98d18e1e7a05472f9a4
Nic Newdigate <nicnewdigate@gmail.com>
2019-02-09 16:22:00 +0000

    Update effect_compressor.h

--- effect_compressor.h
+++ effect_compressor.h
@@ -1,5 +1,5 @@
-#ifndef PAULSTOFFREGEN_EFFECT_COMPRESSOR_H
-#define PAULSTOFFREGEN_EFFECT_COMPRESSOR_H
+#ifndef TEENSYAUDIO_EFFECT_COMPRESSOR_H
+#define TEENSYAUDIO_EFFECT_COMPRESSOR_H
 
 #include <Arduino.h>
 #include "AudioStream.h"
@@ -11,32 +11,27 @@ public:
     AudioEffectCompressor() : AudioStream(1, inputQueueArray) { 
       buildLogTable();
     }
+    
     virtual void update(void);
-
-    uint16_t getThreshold();
-    uint8_t getGainReduction();
+    
     float getInputGain();
-    bool getEnabled();
-
-    void setThreshold(uint16_t threshold);
-    void setGainReduction(uint8_t gainReduction);
     void setInputGain(float inputGain);
+    
+    bool getEnabled();
     void setEnabled(bool enabled);
     void toggleEnabled();
+    
     int16_t interpolate(int16_t input);
-    static bool _initialized;
     
-private:
-    
+private:    
     bool _enabled = true;
-    uint16_t _threshold = 0x5000;
-    uint8_t _gainReduction = 3;
     float _inputGain = 1.0;
     void buildLogTable();
    
     audio_block_t *inputQueueArray[1];
 
     static double logtable[NumlogTablePoints];
+    static bool _initialized;
 };
 
-#endif //PAULSTOFFREGEN_EFFECT_COMPRESSOR_H
+#endif //TEENSYAUDIO_EFFECT_COMPRESSOR_H
